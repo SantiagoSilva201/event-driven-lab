433b604038e7812fc690521ca107e5539197b6ea
SantiagoSilva201 <santiagosilva201@outlook.es>
2026-04-25 18:24:52 +0000

    Finalizacion Laboratorio - Microservicios con Spring Boot, RabbitMQ y Docker

---
+++ producer-service/src/main/java/com/eci/arcn/producer_service/config/RabbitMQConfig.java
@@ -0,0 +1,37 @@
+package com.eci.arcn.producer_service.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${app.rabbitmq.queue}")
+    private String queueName;
+
+    @Value("${app.rabbitmq.exchange}")
+    private String exchangeName;
+
+    @Value("${app.rabbitmq.routingkey}")
+    private String routingKey;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(exchangeName);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+}
